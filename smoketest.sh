3e8ce7cc94819cb4cfc8d8fede030df53b840743
ArunachalamJegadeesan <inboxofarun@gmail.com>
2018-11-29 20:32:13 +0530

    smoketest added

---
+++ smoketest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -x
+curl -X POST -H 'content-type: application/json;charset=UTF-8'  -d '{"productName":"HD Setupbox", "serviceId":"100"}' "$POST_URL" |pbcopy
+pbpaste | grep '\"serviceId\":\"100\"'
+if [ "$?" -eq "0" ] ; then
+echo "Success"
+else
+echo "Failed"
+exit 1
+fi
